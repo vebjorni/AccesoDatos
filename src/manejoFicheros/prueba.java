b150242fd10658018864da24142c8300bcb9e058
vebjorni <raulsanchezgonzalez1@gmail.com>
2019-10-23 13:36:10 +0200

    exportttttt

---
+++ src/manejoFicheros/prueba.java
@@ -0,0 +1,23 @@
+package manejoFicheros;
+
+import java.io.*;
+import java.io.IOException;
+
+public class prueba {
+
+	public static void main(String[] args) throws IOException {
+	
+		try {
+			for (int i = 0; i < 100; i++) {
+				
+				
+				
+			}
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		
+		
+	}
+	
+}
